b97de5dfa6379bf2218ec56a77fb1feac0673f7d
Adria Orenstein <adria.orenstein@gmail.com>
2024-11-16 17:19:12 -0500

    Cleaned up DB folders and moved table creation into separate files

--- server/index.py
+++ server/index.py
@@ -13,8 +13,8 @@ from db.models import test_table
 
 hostName = "localhost"
 serverPort = 8080
-DB_USERNAME = "adriaorenstein"
-DB_PASSWORD = "pg-adria"
+DB_USERNAME = "USERNAME"
+DB_PASSWORD = "PASSWORD"
 
 # ---------- CONNECT TO SERVER ----------
 
