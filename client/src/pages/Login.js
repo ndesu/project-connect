5e6023fe80948ce26d1fa5c7fc5c8c6a31f68d52
Nidhi Desu <nd2290@nyu.edu>
2024-11-20 00:34:02 -0500

    login page layout

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,21 @@ import React from "react";
 
 export default function Login() {
     return (
-        <h1>This is the Login Page</h1>
+        <div class="loginContainer">
+            <h1 class="appTitle">project connect</h1>
+            <div class="signInToggle">
+                <button>Login</button>
+                <p>or</p>
+                <button>Create Account</button>
+            </div>
+            <div class="inputBox">
+                {/* <p>form will be here</p> */}
+                <form>
+                    <label>username</label>
+                    <label>password</label>
+                </form>
+            </div>
+        </div>
+        
     )
 }
